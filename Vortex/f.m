9adcad2e2e198e6301edee00a3839befb0c6a100
Andrew Way <arw405@mun.ca>
2018-04-17 14:01:04 -0230

    working commit. Need to make system of ODE into COLUMNVECTOR=COLUMNFUNCTION

--- Vortex/f.m
+++ Vortex/f.m
@@ -3,6 +3,6 @@ function [ res ] = f( Gamma,q,n )
 %F Summary of this function goes here
 %   Detailed explanation goes here
     
-res=[dHdy(Gamma,q,n)./Gamma,-dHdx(Gamma,q,n)./Gamma];
+res=[fx(Gamma,[q(1:3);q(4:6)],n);fy(Gamma,[q(1:3);q(4:6)],n)];
 end
 
